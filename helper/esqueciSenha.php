544cb341bf4dbb7827f19f10d5f1e0509c598532
Lucas Serafim <lslucas@gmail.com>
2012-02-19 22:23:59 -0600

    helper de esquici minha senha

--- helper/esqueciSenha.php
+++ helper/esqueciSenha.php
@@ -11,8 +11,11 @@ include_once $rp.'_inc/global_function.php';
 include_once $rp.'cadastro/mod.var.php';
 
 
-	$res['login'] = $_REQUEST['login'];
-	if (isset($res['login'])) {
+	$res['login'] = isset($_REQUEST['login']) ? trim($_REQUEST['login']) : null;
+	if (empty($res['login']) || !validaLogin($res['login']))
+		echo 'Login inválido! É permitido apenas letras, números, ponto e _';
+
+	else {
 
 		$sql = "SELECT ${var['pre']}_id, ${var['pre']}_login, ${var['pre']}_email FROM ".TABLE_PREFIX."_${var['path']} ";
 		$sql.=" WHERE ${var['pre']}_login=?";
@@ -24,61 +27,68 @@ include_once $rp.'cadastro/mod.var.php';
 			$qry->bind_result($item, $login, $email);
 			$qry->bind_param('s', $res['login']);
 			$qry->execute();
+			$qry->store_result();
+			$num = $qry->num_rows;
 			$qry->fetch();
 			$qry->close();
 		}
 
-
-		$senha	      = gera_senha(4);
-		$res['senha'] = md5($senha);
-
-		$sql_senha = "UPDATE ".TABLE_PREFIX."_${var['path']} SET ${var['pre']}_senha=?";
-		$sql_senha.=" WHERE ${var['pre']}_id=?";
-
-		if (!$qry_senha=$conn->prepare($sql_senha))
-		   echo $conn->error;
+		if ($num==0 || !validaEmail($email))
+			echo "Usuário não existe!";
 
 		else {
 
-			$qry_senha->bind_param('si', $res['senha'], $item); 
-			$qry_senha->execute();
-			$qry_senha->close();
+			$senha	      = gera_senha(4);
+			$res['senha'] = md5($senha);
 
-			/*
-			 *html do email
-			 */
-			$email_subject = SITE_NAME.": Nova senha de acesso!";
-			$msg = $user_email_header;
-			$msg .= "
-				 <center><img src='".URL_ADMLOGO."'></center><p />
-				 Olá ".$login.", sua senha foi alterada!
+			$sql_senha = "UPDATE ".TABLE_PREFIX."_${var['path']} SET ${var['pre']}_senha=?";
+			$sql_senha.=" WHERE ${var['pre']}_id=?";
 
-				 <p><b>Usuário:</b> ".$login."
-				 <br><b>Senha:</b> ".$senha." 
-				 <br><b>URL:</b> <a href='".SITE_URL."' target='_blank'>".SITE_URL."</a>
-				";
-			$msg .= $user_email_footer;
+			if (!$qry_senha=$conn->prepare($sql_senha))
+			   echo $conn->error;
+
+			else {
+
+				$qry_senha->bind_param('si', $res['senha'], $item); 
+				$qry_senha->execute();
+				$qry_senha->close();
+
+				/*
+				 *html do email
+				 */
+				$email_subject = SITE_NAME.": Nova senha de acesso!";
+				$msg = $user_email_header;
+				$msg .= "
+					 <center><img src='".URL_ADMLOGO."'></center><p />
+					 Olá ".$login.", sua senha foi alterada!
+
+					 <p><b>Usuário:</b> ".$login."
+					 <br><b>Senha:</b> ".$senha." 
+					 <br><b>URL:</b> <a href='".SITE_URL."' target='_blank'>".SITE_URL."</a>
+					";
+				$msg .= $user_email_footer;
 
 
-			/*
-			 *vars to send a email
-			 */
-			$htmlMensage= utf8_decode($msg);
-			$subject	= utf8_decode($email_subject);
-			$fromEmail	= EMAIL;
-			$fromName	= utf8_decode(SITE_NAME);
-			$toName		= utf8_decode($login);
-			$toEmail	= $email;
+				/*
+				 *vars to send a email
+				 */
+				$htmlMensage= utf8_decode($msg);
+				$subject	= utf8_decode($email_subject);
+				$fromEmail	= EMAIL;
+				$fromName	= utf8_decode(SITE_NAME);
+				$toName		= utf8_decode($login);
+				$toEmail	= $email;
 
-			include_once $rp.'inc.sendmail.header.php';
+				include_once $rp.'inc.sendmail.header.php';
 
-			if ($sended)
-				echo 'Nova senha enviada para '.$email;
+				if ($sended)
+					echo 'Nova senha enviada para '.$email;
 
-			else
-				echo 'Senha resetada MAS não foi enviada ao email '.$email;
+				else
+					echo 'Gerado nova senha mas NÃO foi enviada ao email '.$email;
 
-		 }
+			 }
 
+		}
 
 	}
